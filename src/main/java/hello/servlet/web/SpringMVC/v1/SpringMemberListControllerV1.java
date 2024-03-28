508d54a69525f16ec799f9b5d12351a7735b4ac4
jdm2131 <jdm2131@naver.com>
2024-03-28 22:40:59 +0900

    Spring MVC

---
+++ src/main/java/hello/servlet/web/SpringMVC/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,24 @@
+package hello.servlet.web.SpringMVC.v1;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.ModelView;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class SpringMemberListControllerV1 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+    // 저장된 멤버 전부 조회해서 model에 통째로 박아 넣는다 .
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView proceess() {
+        List<Member> members = memberRepository.findAll();
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+        return mv;
+    }
+}
